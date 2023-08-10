c45d01bf67ccd41b4c4719e65dc601f3ebfa8a39
Yi Peng <tanyipeng835@hotmail.com>
2023-08-10 17:26:13 +0800

    working on the about page

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled software developer with Fullstack Development. I have experience in Fullstack Development. I am currenlty 
+        brushing up on my machine learning capabilites by working as a research assistant for SMU on computer vision models to identify malicious permission
+        usage by andriod applicaitons.
+      </motion.p>
+
+     
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/components/Hero.jsx
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import React, { useEffect, useState } from "react";
+import Typed from 'typed.js';
+
+const Hero = () => {
+
+  const el = React.useRef(null);
+
+useEffect(() => {
+  const typed = new Typed(el.current, {
+    strings: [' Aspiring <i>AI &amp; Software Engineer.</i>'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop:true,
+  });
+
+  return () => {
+    // Destroy Typed instance during cleanup to stop animation
+    typed.destroy();
+  };
+}, []);
+
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 `}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Yi Peng</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-1 text-white-100`}>
+           I am a Junior at Singapore Management Univesity <br className='sm:block hidden' />
+
+           <br></br>
+            <span ref ={el} className="font-black text-[#915EFF] lg:text-[60px] sm:text-[40px] xs:text-[30px] text-[25px] lg:leading-[98px] mt-4"></span>
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas/>
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
