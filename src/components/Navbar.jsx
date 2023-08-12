f4d8a8735ee958afad4f73efa91e55e6568cc28e
Yi Peng <tanyipeng835@hotmail.com>
2023-08-12 10:37:48 +0800

    Added Resume Functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,17 +50,24 @@ const Navbar = () => {
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
+        {navLinks.map((nav) => (
+  <li
+    key={nav.id}
+    className={`${
+      active === nav.title ? "text-white" : "text-secondary"
+    } hover:text-white text-[18px] font-medium cursor-pointer`}
+  >
+    {nav.id === "resume" ? (
+      <a href="/Resume.pdf" download>
+        {nav.title}
+      </a>
+    ) : (
+      <a href={`#${nav.id}`} onClick={() => setActive(nav.title)}>
+        {nav.title}
+      </a>
+    )}
+  </li>
+))}
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-end items-center'>
@@ -77,20 +84,24 @@ const Navbar = () => {
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-              {navLinks.map((nav) => (
-                <li
-                  key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(nav.title);
-                  }}
-                >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
-              ))}
+            {navLinks.map((nav) => (
+  <li
+    key={nav.id}
+    className={`${
+      active === nav.title ? "text-white" : "text-secondary"
+    } hover:text-white text-[18px] font-medium cursor-pointer`}
+  >
+    {nav.id === "resume" ? (
+      <a href="/Resume.pdf" download>
+        {nav.title}
+      </a>
+    ) : (
+      <a href={`#${nav.id}`} onClick={() => setActive(nav.title)}>
+        {nav.title}
+      </a>
+    )}
+  </li>
+))}
             </ul>
           </div>
         </div>
